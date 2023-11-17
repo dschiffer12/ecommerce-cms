b4388fb0adce90cf9da2e2f78e49b13ac9e4af59
dschiffer12 <dschiffer12@gmail.com>
2023-11-16 20:38:40 -0700

    Finished colors API, built products route

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -43,6 +43,12 @@ export function MainNav({
 
         },
         {
+            href: `/${params.storeId}/products`,
+            label: 'Products',
+            active: pathname === `/${params.storeId}/products`,
+
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`,
